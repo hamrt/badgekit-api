d0d908432404e84556a8035001337cfa8fde211e
hamrt <ronald.ham@gmail.com>
2016-10-18 16:00:23 +0200

    Update program.js

--- app/models/program.js
+++ app/models/program.js
@@ -51,10 +51,10 @@ Programs.toResponse = function toResponse(row) {
 
 Programs.validateRow = makeValidator({
   id: optional('isInt'),
-  slug: required('len', 1, 255),
-  name: required('len', 1, 255),
+  slug: required('isLength', 1, 255),
+  name: required('isLength', 1, 255),
   url: required('isUrl'),
-  description: optional('len', 0, 255),
+  description: optional('isLength', 0, 255),
   email: optional('isEmail'),
   imageId: optional('isInt'),
   issuerId: optional('isInt'),
